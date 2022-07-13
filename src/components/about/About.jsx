3ec1d649464c18a1d010ed68decd3de9685fe774
Pacifique Kwizera <kwizerapacifique19@gmail.com>
2022-07-13 15:54:43 +0200

    2hr 4min

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,30 +1,53 @@
-import React from 'react'
-import './about.css'
-import me from '../../assets/me.png'
+import React from "react";
+import "./about.css";
+import me from "../../assets/me-about.jpg";
+import { FaAward } from "react-icons/fa";
+import { TbUsers } from "react-icons/tb";
+import { RiFolderChartFill } from "react-icons/ri";
 
 const About = () => {
   return (
-    <section id='about'>
-      <h5>Get to know</h5>
-      <h1>About Me</h1>
+    <section id="about">
+      <h5>Get to know me</h5>
+      <h2>About me</h2>
+
       <div className="container about__container">
         <div className="about__me">
           <div className="about__me-image">
-            <img src={me} alt="About image" />
+            <img src={me} alt="about-me image" />
           </div>
         </div>
-      </div>
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <FaAward className="about__icon" />
+              <h5>Expereince</h5>
+              <small>3+ years of work experience</small>
+            </article>
 
-      <div className="about__content">
-        <div className="about__cards">
-          <article className='about__card'>
-            <h5>Experience</h5>
-            <small>3+ years</small>
-          </article>
+            <article className="about__card">
+              <TbUsers className="about__icon" />
+              <h5>Clients</h5>
+              <small>200+ clients worldwide</small>
+            </article>
+
+            <article className="about__card">
+              <RiFolderChartFill className="about__icon" />
+              <h5>Projects</h5>
+              <small>200+ projects completed</small>
+            </article>
+          </div>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+            ipsum optio ducimus vitae itaque laudantium inventore velit illo
+            voluptas consequuntur, quae aperiam blanditiis. Obcaecati minima
+            maiores maxime quis excepturi doloremque.
+          </p>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default About
+export default About;
--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,12 +1,139 @@
-import React from 'react'
-import './services.css'
+import React from "react";
+import "./services.css";
+import { GiCheckMark } from "react-icons/gi";
 
 const Services = () => {
   return (
-    <section id='services'>
-      Services
-    </section>
-  )
-}
+    <section id="services">
+      <div className="container services__container">
+        <article className="service">
+          <div className="service__head">
+            <h3>UI/UX Design</h3>
+          </div>
+          <ul className="service__list">
+            <li>
+              <GiCheckMark />
+              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+            </li>
+            <li>
+              <GiCheckMark />
+              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+            </li>
+            <li>
+              <GiCheckMark />
+              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+            </li>
+            <li>
+              <GiCheckMark />
+              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+            </li>
+            <li>
+              <GiCheckMark />
+              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+            </li>
+            <li>
+              <GiCheckMark />
+              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+            </li>
+            <li>
+              <GiCheckMark />
+              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+            </li>
+            <li>
+              <GiCheckMark />
+              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+            </li>
+          </ul>
+        </article>
+        {/* end of ui/ux */}
 
-export default Services
+        {/*  web development */}
+        <article className="service">
+          <div className="service__head">
+            <h3>Web Development</h3>
+          </div>
+          <ul className="service__list">
+            <li>
+              <GiCheckMark />
+              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+            </li>
+            <li>
+              <GiCheckMark />
+              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+            </li>
+            <li>
+              <GiCheckMark />
+              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+            </li>
+            <li>
+              <GiCheckMark />
+              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+            </li>
+            <li>
+              <GiCheckMark />
+              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+            </li>
+            <li>
+              <GiCheckMark />
+              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+            </li>
+            <li>
+              <GiCheckMark />
+              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+            </li>
+            <li>
+              <GiCheckMark />
+              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+            </li>
+          </ul>
+        </article>
+        {/* End of web development */}
+
+
+        {/* Content creation */}
+        <article className="service">
+          <div className="service__head">
+            <h3>Content Creation</h3>
+          </div>
+          <ul className="service__list">
+            <li>
+              <GiCheckMark />
+              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+            </li>
+            <li>
+              <GiCheckMark />
+              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+            </li>
+            <li>
+              <GiCheckMark />
+              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+            </li>
+            <li>
+              <GiCheckMark />
+              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+            </li>
+            <li>
+              <GiCheckMark />
+              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+            </li>
+            <li>
+              <GiCheckMark />
+              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+            </li>
+            <li>
+              <GiCheckMark />
+              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+            </li>
+            <li>
+              <GiCheckMark />
+              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+            </li>
+          </ul>
+        </article>
+        {/* end of content creation */}
+      </div>
+    </section>
+  );
+};
+
+export default Services;
